a30f1225b1e2418c4bd55c69f6eb1389c9adc78a
wabdsgn <wab2com@gmail.com>
2017-02-19 17:00:21 +0100

    rectification sur les sections

--- web/app/themes/cedar-architect/templates/page-sections.php
+++ web/app/themes/cedar-architect/templates/page-sections.php
@@ -7,7 +7,7 @@ while (have_posts()) : the_post(); ?>
 
   <div class="row">
 
-    <div class="columns large-8">
+    <div class="columns large-8 large-centered">
       <div class="page-wrapper">
         <?php get_template_part('partials/content-page'); ?>
       </div>
@@ -29,19 +29,15 @@ while (have_posts()) : the_post(); ?>
     ?>
 
     <div class="section">
-
-      <div class="page-section-row">
-
-        <div class="page-section-column <?php if( empty($image) ) { echo 'without-image';} ?>">
-         <h2 class="page-section--title"><span><?php echo $title; ?></span></h2>
+      <div class="row">
+        <div class="columns large-10 large-centered">
+          <h2 class="page-section--title"><span><?php echo $title; ?></span></h2>
+          <?php if( !empty($image) ) : ?>
+            <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>" class="main-image" />
+          <?php endif; ?>
           <?php echo $content; ?>
         </div>
-        <?php if( !empty($image) ): ?>
-          <div class="page-section-column"><img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>" /></div>
-        <?php endif; ?>
-
       </div>
-
     </div>
 
   <?php endwhile; ?>
