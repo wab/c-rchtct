8a40a9306cdae04b7dd730ec586d9a15e60ffeca
wabdsgn <wab2com@gmail.com>
2017-02-27 22:58:13 +0100

    réseaux sociaux en target _blank

--- web/app/themes/cedar-architect/templates/partials/footer.php
+++ web/app/themes/cedar-architect/templates/partials/footer.php
@@ -52,11 +52,11 @@
     <div class="widget">
       <p class="widget--title"><?php _e('Follow us', 'cedar-architect'); ?></p>
       <ul class="reseaux--list no-bullet scrollreveal">
-        <li class="reseaux--item"><button class="reseaux--link" onclick="window.location.href='https://twitter.com/mysketcher'"><svg class="icon-twitter"><use xlink:href="#icon-twitter"></use></svg></button></li>
-        <li class="reseaux--item"><button class="reseaux--link" onclick="window.location.href='https://www.facebook.com/My-Sketcher-1548463242070456'"><svg class="icon-facebook"><use xlink:href="#icon-facebook"></use></svg></button></li>
-        <li class="reseaux--item"><button class="reseaux--link" onclick="window.location.href='https://plus.google.com/118015676047486854511'"><svg class="icon-google-plus"><use xlink:href="#icon-google-plus"></use></svg></button></li>
-        <li class="reseaux--item"><button class="reseaux--link" onclick="window.location.href='https://www.youtube.com/user/MrMySketcher'"><svg class="icon-youtube-play"><use xlink:href="#icon-youtube-play"></use></svg></button></li>
-        <li class="reseaux--item"><button class="reseaux--link" onclick="window.location.href='https://fr.pinterest.com/mysketcher/'"><svg class="icon-pinterest"><use xlink:href="#icon-pinterest"></use></svg></button></li>
+        <li class="reseaux--item"><button class="reseaux--link" onclick="window.open('https://twitter.com/mysketcher')"><svg class="icon-twitter"><use xlink:href="#icon-twitter"></use></svg></button></li>
+        <li class="reseaux--item"><button class="reseaux--link" onclick="window.open('https://www.facebook.com/My-Sketcher-1548463242070456')"><svg class="icon-facebook"><use xlink:href="#icon-facebook"></use></svg></button></li>
+        <li class="reseaux--item"><button class="reseaux--link" onclick="window.open('https://plus.google.com/118015676047486854511')"><svg class="icon-google-plus"><use xlink:href="#icon-google-plus"></use></svg></button></li>
+        <li class="reseaux--item"><button class="reseaux--link" onclick="window.open('https://www.youtube.com/user/MrMySketcher')"><svg class="icon-youtube-play"><use xlink:href="#icon-youtube-play"></use></svg></button></li>
+        <li class="reseaux--item"><button class="reseaux--link" onclick="window.open('https://fr.pinterest.com/mysketcher/')"><svg class="icon-pinterest"><use xlink:href="#icon-pinterest"></use></svg></button></li>
       </ul>
     </div>
   </div>
