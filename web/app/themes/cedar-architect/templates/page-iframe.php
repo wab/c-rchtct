06d5824755d8d6237186a7b875171d803acd88f3
wabdsgn <wab2com@gmail.com>
2017-03-12 15:10:35 +0100

    majs lang & rectification de la redirection iframe.

--- web/app/themes/cedar-architect/templates/page-iframe.php
+++ web/app/themes/cedar-architect/templates/page-iframe.php
@@ -5,6 +5,6 @@
 ?>
 <?php while (have_posts()) : the_post(); ?>
   <?php if (get_field('iframe_url')) : ?>
-    <iframe src="<?php the_field('iframe_url'); ?>" frameborder="0" width="100%" height="800px"></iframe>
+    <iframe src="https://support.cedar-architect.com/<?php echo pll_current_language() ?>/support/home" frameborder="0" width="100%" height="800px"></iframe>
   <?php endif;  ?>
 <?php endwhile; ?>
