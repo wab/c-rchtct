7ae15fe296ee880bac6a5ccd46d71c81e63194e9
wabdsgn <wab2com@gmail.com>
2017-06-28 23:08:40 +0200

    rectifs sur les sections

--- web/app/themes/cedar-architect/templates/partials/sections.php
+++ web/app/themes/cedar-architect/templates/partials/sections.php
@@ -13,15 +13,21 @@
 
       <div class="section">
         <div class="section-wrapper">
-          <h2 class="page-section--title"><span><?php echo $title; ?></span></h2>
+          <h2 class="section-title"><span><?php echo $title; ?></span></h2>
           <?php if( $media == 'video' && !empty($videoid) ) : ?>
             <div class="section-video">
-              <iframe width="560" height="315" src="https://www.youtube.com/embed/<?php echo $videoid; ?>?rel=0" frameborder="0" allowfullscreen></iframe>
+              <div class="responsive-embed ">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/<?php echo $videoid; ?>?rel=0" frameborder="0" allowfullscreen></iframe>
+              </div>
             </div>
           <?php elseif( $media == 'image' && !empty($image) ) : ?>
-            <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>" class="section-thumbnail" />
+            <div class="section-thumbnail">
+              <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>" />
+            </div>
           <?php endif; ?>
-          <?php echo $content; ?>
+          <div class="section-content">
+            <?php echo $content; ?>
+          </div>
         </div>
       </div>
 
