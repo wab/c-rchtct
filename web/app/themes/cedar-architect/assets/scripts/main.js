122435a5d434ec4f1a4ae4b5204472efe48c0a72
wabdsgn <wab2com@gmail.com>
2017-09-16 18:33:18 +0200

    correction include js foundation

--- web/app/themes/cedar-architect/assets/scripts/main.js
+++ web/app/themes/cedar-architect/assets/scripts/main.js
@@ -3,20 +3,7 @@ import 'jquery';
 
 // Import npm dependencies
 import 'svgxuse';
-import 'foundation-sites/js/foundation.core';
-import 'foundation-sites/js/foundation.util.mediaQuery';
-import 'foundation-sites/js/foundation.util.keyboard';
-import 'foundation-sites/js/foundation.util.box';
-import 'foundation-sites/js/foundation.util.nest';
-import 'foundation-sites/js/foundation.util.triggers';
-import 'foundation-sites/js/foundation.util.motion';
-import 'foundation-sites/js/foundation.responsiveToggle';
-import 'foundation-sites/js/foundation.dropdown';
-import 'foundation-sites/js/foundation.reveal';
-import 'foundation-sites/js/foundation.sticky';
-import 'foundation-sites/js/foundation.accordionMenu';
-import 'foundation-sites/js/foundation.magellan';
-import 'foundation-sites/js/foundation.tooltip';
+import 'foundation-sites/dist/js/foundation';
 
 // import local dependencies
 import Router from './util/router';
