f9f06da558a0206f812e940c3f7f7f3aaec75665
wabdsgn <wab2com@gmail.com>
2017-11-07 10:55:21 +0100

    mofification anchor page tuto

--- web/app/themes/cedar-architect/templates/page-tutoriels.php
+++ web/app/themes/cedar-architect/templates/page-tutoriels.php
@@ -2,58 +2,61 @@
 /**
  * Template Name: Page tutoriels
  */
-use Cocur\Slugify\Slugify;
-
-$slugify = new Slugify();
 
 ?>
 
-<?php while (have_posts()) : the_post(); ?>
+<?php while (have_posts()) :
+    the_post(); ?>
 
   <div class="page-wrapper" >
     <div class="page-column page-contenu" id="page-contenu">
-      <?php get_template_part('partials/content-page'); ?>
-      <?php if( have_rows('tutoriels') ): ?>
+        <?php get_template_part('partials/content-page'); ?>
+        <?php if (have_rows('tutoriels')) : ?>
 
-         <?php while ( have_rows('tutoriels') ) : the_row(); ?>
+            <?php $num = 0; ?>
+            <?php while (have_rows('tutoriels')) :
+                the_row(); ?>
 
-           <?php
+            <?php
               //vars
+              $num = $num + 1;
               $title = get_sub_field('title');
               $pdf = get_sub_field('pdf');
-              $slugtitle = $slugify->slugify($title);
               $shortcode = '[gview file="'. $pdf . '" title="'. $title .'" save="1" cache="1"]';
             ?>
-            <section class="tutoriel--container" id="<?php echo $slugtitle; ?>" data-magellan-target="<?php echo $slugtitle; ?>">
+            <section class="tutoriel--container" id="<?php echo $num; ?>" data-magellan-target="<?php echo $num; ?>">
               <h2 class="tutoriel--title"><?php echo $title; ?></h2>
               <div class="tutoriel--contenu">
                 <?php echo do_shortcode($shortcode); ?>
               </div>
             </section>
-          <?php endwhile; ?>
+            <?php endwhile; ?>
 
-      <?php endif; ?>
+        <?php endif; ?>
 
     </div>
     <div class="page-column sidebar" data-sticky-container>
       <nav class="tutoriels--navigation sticky show-for-large" data-sticky data-anchor="page-contenu">
         <h2>Liste des tutoriaux</h2>
-        <?php if( have_rows('tutoriels') ): ?>
+        <?php if (have_rows('tutoriels')) : ?>
+
+            <?php $numLink = 0; ?>
 
         <ul class="tutoriel--navigation--list menu vertical" data-magellan>
-         <?php while ( have_rows('tutoriels') ) : the_row(); ?>
+            <?php while (have_rows('tutoriels')) :
+                the_row(); ?>
 
             <?php
               //vars
               $title = get_sub_field('title');
-              $slugtitle = $slugify->slugify($title);
+              $numLink = $numLink + 1;
             ?>
 
-            <li class="tutoriel--navigation--item"><a class="tutoriel--navigation--link" href="#<?php echo $slugtitle; ?>"><?php echo $title; ?></a></li>
+            <li class="tutoriel--navigation--item"><a class="tutoriel--navigation--link" href="#<?php echo $numLink; ?>"><?php echo $title; ?></a></li>
 
-          <?php endwhile; ?>
+            <?php endwhile; ?>
         </ul>
-      <?php endif; ?>
+        <?php endif; ?>
       </nav>
     </div>
 
