9f0c5ba80edf73dd1797382c31bbc2247ede84a9
wabdsgn <wab2com@gmail.com>
2017-11-08 19:45:04 +0100

    suppression du tel dans la page contact

--- web/app/themes/cedar-architect/templates/page-contact.php
+++ web/app/themes/cedar-architect/templates/page-contact.php
@@ -4,10 +4,11 @@
  */
 ?>
 
-<?php while (have_posts()) : the_post(); ?>
+<?php while (have_posts()) :
+    the_post(); ?>
   <div class="section">
     <div class="row text-center">
-      <div class="columns large-4 contact--address">
+      <div class="columns large-6 contact--address">
          <svg viewBox="0 0 100 100" class="pros--icon">
             <circle class="bg" opacity=".2" cx="50" cy="50" r="50"/>
             <circle class="bg" opacity=".6" cx="50" cy="50" r="44"/>
@@ -18,11 +19,11 @@
               <path d="M49.7 28.6c2.4 0 4.3 1.9 4.3 4.3s-1.9 4.3-4.3 4.3-4.3-1.9-4.3-4.3 1.9-4.3 4.3-4.3zm0 10.1c3.2 0 5.9-2.6 5.9-5.9S52.9 27 49.7 27s-5.9 2.6-5.9 5.9 2.7 5.8 5.9 5.8zm2.9 13.8c-.4.1-.7.5-.6.9.1.4.4.6.8.6h.2c.5-.1 1-.2 1.5-.2.4-.1.8-.5.7-.9-.1-.4-.5-.8-.9-.7-.6.1-1.1.2-1.7.3zm4.2.3c0 .4.4.8.8.8l1.6-.1c.4 0 .8-.4.8-.8s-.4-.8-.8-.8l-1.7.1c-.4 0-.7.4-.7.8zm-11.4 3.7c-.4.3-.8.6-1.3.9-.4.2-.5.7-.2 1.1.2.2.4.4.7.4.1 0 .3 0 .4-.1.5-.3.9-.6 1.4-.9.4-.3.4-.8.2-1.1-.3-.5-.8-.5-1.2-.3zm17.9-5.2c-.3.2-.6.3-1.1.4-.4.1-.7.5-.6.9.1.4.4.6.8.6h.2c.7-.1 1.2-.4 1.7-.7l.1-.1c.4-.3.4-.8.2-1.1-.4-.3-.9-.4-1.3 0zm-28.7 6.2c.1-.4-.1-.9-.6-1-.4-.1-.9.1-1 .6-.1.4-.2.8-.2 1.1v.8c0 .4.4.7.8.7h.1c.4 0 .8-.4.7-.9v-.5c.1-.3.1-.5.2-.8zM63.3 45c-.6-.1-1.3 0-1.9.1-.4.1-.7.6-.6 1 .1.4.4.6.8.6h.2c.4-.1.8-.2 1.3-.1.4.1.8-.2.9-.7 0-.4-.3-.8-.7-.9zm1.6 4c-.1.4.1.9.5 1h.2c.3 0 .7-.2.8-.6.1-.3.1-.5.1-.8v-.2c0-.4-.1-.8-.2-1.1-.2-.4-.6-.6-1-.5-.4.2-.6.6-.5 1 .1.2.1.4.1.6v.6zm-23.4 9.9c-.5.2-.9.4-1.4.6-.4.2-.6.6-.5 1 .1.3.4.5.7.5.1 0 .2 0 .3-.1.5-.2 1-.4 1.5-.7.4-.2.6-.7.4-1.1-.1-.2-.6-.4-1-.2zm-5.8-3.3c.1 0 .3 0 .4-.1.2-.1.5-.2.7-.2h.3c.4.1.8-.2.9-.7.1-.4-.3-.8-.7-.9h-.7c-.5.1-.9.2-1.3.4s-.5.7-.3 1.1c.1.2.4.4.7.4zm-.2 6.3c.2 0 .3.1.5.1h1.4c.4-.1.7-.5.7-.9-.1-.4-.5-.7-.9-.7-.3 0-.6.1-1 0-.1 0-.2 0-.3-.1-.4-.1-.9.1-1 .6-.1.4.2.9.6 1z"/>
             </g>
           </svg>
-      <?php if(get_field('address')): ?>
+        <?php if (get_field('address')) : ?>
         <address><?php the_field('address'); ?></address>
-      <?php endif; ?>
+        <?php endif; ?>
       </div>
-      <div class="columns large-4 contact--phone">
+      <!-- <div class="columns large-4 contact--phone">
         <svg viewBox="0 0 100 100" class="pros--icon">
           <circle class="bg" opacity=".2" cx="50" cy="50" r="50"/>
           <circle class="bg" opacity=".6" cx="50" cy="50" r="44"/>
@@ -35,11 +36,11 @@
           </g>
         </svg>
         <p>
-      <?php if(get_field('phone')): ?>
+        <?php if (get_field('phone')) : ?>
         <div><button class="callbutton button has-tip top" data-tooltip aria-haspopup="true" data-disable-hover="true" tabindex="1" title="<?php the_field('phone'); ?>">Afficher le numéro</button></div>
-      <?php endif; ?>
-      </div>
-      <div class="columns large-4 contact--opening">
+        <?php endif; ?>
+      </div> -->
+      <div class="columns large-6 contact--opening">
         <svg viewBox="0 0 100 100" class="pros--icon">
           <circle class="bg" opacity=".2" cx="50" cy="50" r="50"/>
           <circle class="bg" opacity=".6" cx="50" cy="50" r="44"/>
@@ -49,9 +50,9 @@
             <path d="M49 50.5c-.9 0-1.6-.7-1.6-1.6 0-.9.7-1.6 1.6-1.6s1.6.7 1.6 1.6c0 .9-.7 1.6-1.6 1.6zm.8-4.7v-8.1c0-.4-.4-.8-.8-.8s-.8.4-.8.8v8.1c-1.1.3-2 1.2-2.3 2.3h-5.7c-.4 0-.8.4-.8.8s.4.8.8.8h5.7c.4 1.4 1.6 2.4 3.1 2.4 1.8 0 3.2-1.4 3.2-3.2 0-1.5-1-2.7-2.4-3.1zM49 32.9c.4 0 .8-.4.8-.8v-.8c0-.4-.4-.8-.8-.8s-.8.4-.8.8v.8c0 .4.4.8.8.8zm0 32c-.4 0-.8.4-.8.8v.8c0 .4.4.8.8.8s.8-.4.8-.8v-.8c0-.4-.4-.8-.8-.8zm17.6-16.8h-.8c-.4 0-.8.4-.8.8s.4.8.8.8h.8c.4 0 .8-.4.8-.8s-.4-.8-.8-.8zm-34.4 0h-.8c-.4 0-.8.4-.8.8s.4.8.8.8h.8c.4 0 .8-.4.8-.8s-.4-.8-.8-.8zm28.7-12.2l-.6.6c-.3.3-.3.8 0 1.1.2.2.4.2.6.2.2 0 .4-.1.6-.2l.5-.6c.3-.3.3-.8 0-1.1-.3-.3-.8-.3-1.1 0zM36.6 60.2l-.6.6c-.3.3-.3.8 0 1.1.2.2.4.2.6.2.2 0 .4-.1.6-.2l.6-.6c.3-.3.3-.8 0-1.1-.4-.3-.9-.3-1.2 0zm24.8 0c-.3-.3-.8-.3-1.1 0-.3.3-.3.8 0 1.1l.6.6c.2.2.4.2.6.2.2 0 .4-.1.6-.2.3-.3.3-.8 0-1.1l-.7-.6zM37.1 35.9c-.3-.3-.8-.3-1.1 0-.3.3-.3.8 0 1.1l.6.6c.2.2.4.2.6.2.2 0 .4-.1.6-.2.3-.3.3-.8 0-1.1l-.7-.6z"/>
           </g>
         </svg>
-      <?php if(get_field('opening')): ?>
+        <?php if (get_field('opening')) : ?>
         <p class="opening"><?php the_field('opening'); ?></p>
-      <?php endif; ?>
+        <?php endif; ?>
       </div>
     </div>
   </div>
@@ -70,7 +71,6 @@
 
 <script type="text/javascript">
   /* global google */
-  console.log('test');
   function initMap() {
     const styles = [
       {
