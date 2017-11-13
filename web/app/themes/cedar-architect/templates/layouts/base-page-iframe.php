3946f9bf19275caa430aeae2f6fb37eca1193746
wabdsgn <wab2com@gmail.com>
2017-11-13 23:36:32 +0100

    suppression olark page iframe

--- web/app/themes/cedar-architect/templates/layouts/base-page-iframe.php
+++ web/app/themes/cedar-architect/templates/layouts/base-page-iframe.php
@@ -1,6 +1,6 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
-  <?php get_template_part('partials/head'); ?>
+    <?php get_template_part('partials/head'); ?>
   <body <?php body_class(); ?>>
     <!-- Google Tag Manager (noscript) -->
     <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PHRJPQ"
@@ -9,18 +9,18 @@
     <div class="wrap">
       <!--[if IE]>
         <div class="alert alert-warning">
-          <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'sage'); ?>
+            <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'sage'); ?>
         </div>
       <![endif]-->
-      <?php
+        <?php
         do_action('get_header');
         get_template_part('partials/header');
-      ?>
+        ?>
 
-      <?php get_template_part('partials/page-header'); ?>
+        <?php get_template_part('partials/page-header'); ?>
       <div class="container" role="document">
         <main class="main">
-          <?php include App\template()->main(); ?>
+            <?php include App\template()->main(); ?>
         </main>
         <?php if (App\display_sidebar()) : ?>
           <aside class="sidebar">
@@ -28,12 +28,12 @@
           </aside>
         <?php endif; ?>
       </div>
-      <?php
+        <?php
         do_action('get_footer');
         get_template_part('partials/icons');
-        get_template_part('partials/olark');
+        // get_template_part('partials/olark');
         wp_footer();
-      ?>
+        ?>
     </div>
   </body>
 </html>
